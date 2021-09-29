e45e1853d92baa38ef77b98a5c64107cc625d642
bonnett93 <bonnettjordan@gmail.com>
2021-09-28 19:11:56 -0500

    Fix import library datetime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """Module: 1-pack_web_static.py"""
 from fabric.api import local
-from datetime import date
+from datetime import datetime
+
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
+
+    local('mkdir -p ./versions')
+    today = datetime.now()
+    print(today)
+    year = str(today.year)
+    month = str(today.month)
+    day = str(today.day)
+    hour = str(today.hour)
+    minute = str(today.minute)
+    second = str(today.second)
+    name = 'web_static_'+year+month+day+hour+minute+second+'.tgz'
+
     try:
-        today = date.now()
-        year = str(today.year)
-        month = str(today.month)
-        day = str(today.day)
-        hour = str(today.hour)
-        minute = str(today.minute)
-        second = str(today.second)
-
-        name = 'web_static_'+year+month+day+hour+minute+second+'.tgz'
-
-        local('mkdir -p ./versions')
         local('tar -cvzf versions/{} ./web_static'.format(name))
-
         return 'versions/{}'.format(name)
 
     except:
