48b7df7989ca4ba09a20b7d8cd94c7304053c548
bonnett93 <bonnettjordan@gmail.com>
2021-10-06 15:13:58 -0500

    Upload first 3 files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 0-hello: starts a Flask web application
+    listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
