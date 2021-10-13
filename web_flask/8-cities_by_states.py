332e96be37aad5fc186a9bf787b85b548b748399
bonnett93 <bonnettjordan@gmail.com>
2021-10-13 18:56:02 -0500

    Upload task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def removeSQLAlchemySession(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def stateList():
+    """display a HTML page: (inside the tag BODY)"""
+    states_all = storage.all(State).values()
+    states = sorted(states_all, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+@app.route('/cities_by_states', strict_slashes=False)
+def city_by_state():
+    """display a HTML page: (inside the tag BODY)"""
+    states_all = storage.all(State).values()
+    states = sorted(states_all, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
